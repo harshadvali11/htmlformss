ef7d97d0dcae26d42422eb9a29e9aeb4be5e87aa
harshadvali11 <harshadvali1431@gmail.com>
2022-12-20 18:31:52 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -27,6 +27,27 @@ def insert_webpage(request):
         return HttpResponse('Webpage is inserted se=uccessfully')
     return render(request,'insert_webpage.html',d)
 
+def select_topic(request):
+    topics=Topic.objects.all()
+    d={'topics':topics}
+
+    if request.method=='POST':
+        tn=request.POST.getlist('topic')
+        print(tn)
+        webpages=Webpage.objects.none()
+        for i in tn:
+            webpages=webpages|Webpage.objects.filter(topic_name=i)
+        data={'webpages':webpages}
+        return render(request,'display_webpage.html',data)
+    return render(request,'select_topic.html',d)
+
+def checkbox(request):
+    topics=Topic.objects.all()
+    d={'topics':topics}
+    
+    return render(request,'checkbox.html',d)
+
+
 
 
 
--- project25/urls.py
+++ project25/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('insert_topic/',insert_topic,name='insert_topic'),
     path('insert_webpage/',insert_webpage,name='insert_webpage'),
+    path('select_topic/',select_topic,name='select_topic'),
+    path('checkbox/',checkbox,name='checkbox'),
 ]
 
 
